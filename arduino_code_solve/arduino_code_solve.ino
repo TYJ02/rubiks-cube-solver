b1df17c22937b5618d1b861b2c8b1942fa7bc0ae
TYJ01 <yujing2788@gmail.com>
2024-01-13 22:44:21 +0800

    updated arduino solve code

--- arduino_code_solve/arduino_code_solve.ino
+++ arduino_code_solve/arduino_code_solve.ino
@@ -5,120 +5,213 @@
 const int stepsPerRevolution = 200 ;
 const int stepstaken = 50; //90 degree
 // Create Instance of Stepper library
-Stepper mystepper_1(stepsPerRevolution, 8, 9, 10, 11);
-Stepper mystepper_2(stepsPerRevolution, 4, 5, 6, 7);
-Servo myservo_1;
-Servo myservo_2;
+Servo servo_down;
+Servo servo_front;
+Servo servo_right;
+Servo servo_left;
+Servo servo_back;
 
 
-void ST1a()
-{
-  // step one revolution in one direction:
-  //Serial.println("stepper motor front counterclockwise");
-  mystepper_1.step(stepstaken);
-  delay(500);
-}
-
-void ST1()
-{
-  // step one revolution in the other direction:
-  mystepper_1.step(-stepstaken);
-  delay(500);
-}
-
-void ST2a()
-{
-  // step one revolution in one direction:
-  //Serial.println("stepper motor back counterclockwise");
-  mystepper_2.step(stepstaken);
-  delay(500);
-}
-
-void ST2()
-{
-  // step one revolution in the other direction:
-  //Serial.println("stepper motor back clockwise");
-  mystepper_2.step(-stepstaken);
-  delay(500);
-}
-
-void rt()
-{
-  //Serial.println("rotate clockwise");
-  for(int a=0; a<=10; a++)
-  {
-  mystepper_1.step(a);
-  mystepper_2.step(a);
-  }
-}
-
-void rta()
-{
-  //Serial.println("rotate counterclockwise");
-  for(int s=0; s<10; s++)
-  {
-  mystepper_1.step(-s);
-  mystepper_2.step(-s);
-  }
-}
-
-
-void SV1()
+void F()
 {
   //Serial.println("arm clockwise");
-  for(int pos = 50; pos <= 180; pos += 4)
-  {                                  
-    myservo_1.write(pos);      
-    delay(15);
-    //myservo_1.detach();
-    
+  servo_front.write(135);
+  delay(150);
+  servo_front.write(90);
+  delay(1000);
   }
   
-}
 
-void SV1a()
+void F2()
 {
-  //Serial.println("arm anticlockwise");
-  for(int pos = 180; pos >=50; pos -= 4) 
-  {                                  
-    myservo_1.write(pos);      
-    delay(15);
-    //myservo.detach();
-    
-  }
-}
+    serial_front.write(135);
+    delay(300);
+    servo_front.write(90);
+    delay(1000);
+    }
 
-void SV2a()
+
+void AF()
 {
-  //Serial.println("arm engage");
-  for(int pos = 0; pos <= 180; pos += 5) 
-    myservo_2.write(pos);      
-    delay(15);
-    //myservo.detach();
+  //Serial.println("arm clockwise");
+  servo_front.write(45);
+  delay(150);
+  servo_front.write(90);
+  delay(1000);
   }
+  
 
-
-void SV2()
+void AF2()
 {
-  //Serial.println("arm disengage");
-  for(int pos = 360; pos >=0; pos -= 5) 
-  {                                  
-    myservo_2.write(pos);      
-    delay(15);
-    //myservo.detach();
-    
+    serial_front.write(45);
+    delay(300);
+    servo_front.write(90);
+    delay(1000);
+    }
+
+
+void R()
+{
+  //Serial.println("arm clockwise");
+  servo_right.write(135);
+  delay(150);
+  servo_right.write(90);
+  delay(1000);
   }
-}
+  
+
+void R2()
+{
+    serial_right.write(135);
+    delay(300);
+    servo_right.write(90);
+    delay(1000);
+    }
+
+
+void AR()
+{
+  //Serial.println("arm clockwise");
+  servo_right.write(45);
+  delay(150);
+  servo_right.write(90);
+  delay(1000);
+  }
+  
+
+void AR2()
+{
+    serial_right.write(45);
+    delay(300);
+    servo_right.write(90);
+    delay(1000);
+    }
+
+
+void B()
+{
+  //Serial.println("arm clockwise");
+  servo_back.write(135);
+  delay(150);
+  servo_back.write(90);
+  delay(1000);
+  }
+  
+
+void B2()
+{
+    serial_back.write(135);
+    delay(300);
+    servo_back.write(90);
+    delay(1000);
+    }
+
+
+void AB()
+{
+  //Serial.println("arm clockwise");
+  servo_back.write(45);
+  delay(150);
+  servo_back.write(90);
+  delay(1000);
+  }
+  
+
+void AB2()
+{
+    serial_back.write(45);
+    delay(300);
+    servo_back.write(90);
+    delay(1000);
+    }
+
+
+void L()
+{
+  //Serial.println("arm clockwise");
+  servo_left.write(135);
+  delay(150);
+  servo_left.write(90);
+  delay(1000);
+  }
+  
+
+void L2()
+{
+    serial_left.write(135);
+    delay(300);
+    servo_left.write(90);
+    delay(1000);
+    }
+
+
+void AL()
+{
+  //Serial.println("arm clockwise");
+  servo_left.write(45);
+  delay(150);
+  servo_left.write(90);
+  delay(1000);
+  }
+  
+
+void AL2()
+{
+    serial_left.write(45);
+    delay(300);
+    servo_left.write(90);
+    delay(1000);
+    }
+
+
+void D()
+{
+  //Serial.println("arm clockwise");
+  servo_down.write(135);
+  delay(150);
+  servo_down.write(90);
+  delay(1000);
+  }
+  
+
+void D2()
+{
+    serial_down.write(135);
+    delay(300);
+    servo_down.write(90);
+    delay(1000);
+    }
+
+
+void AD()
+{
+  //Serial.println("arm clockwise");
+  servo_down.write(45);
+  delay(150);
+  servo_down.write(90);
+  delay(1000);
+  }
+  
+
+void AD2()
+{
+    serial_down.write(45);
+    delay(300);
+    servo_down.write(90);
+    delay(1000);
+    }
+
 
 void setup() {
 
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
   // set the speed at 60 rpm:
-  mystepper_1.setSpeed(60);
-  mystepper_2.setSpeed(60);
-  myservo_1.attach(12);
-  myservo_2.attach(13);
+  servo_bottom.attach(2);
+  servo_front.attach(3);
+  servo_right.attach(4);
+  servo_left.attach(5);
+  servo_back.attach(6);
   Serial.begin(19200);
   //Serial.setTimeout(1);
 }
@@ -144,62 +237,97 @@ void loop() {
         else
             Serial.println("none");
     }
+    else
+    {
+        F();
+    }
     
     if (j == 1){
         digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
         delay(1000);                      // wait for a second
         digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
         delay(1000);
+    
         for (i=0; i<y.length(); i++){
             if (y[i] == 'A')
-                {
-                ST1();
-                
+            {
+                F();    
             }
             else if  (y[i] == 'B')
             {
-                ST1a();
-                
+                F2(); 
             }
             else if  (y[i] == 'C')
             {
-                ST2();
-                
+                AF();
             }
             else if  (y[i] == 'D')
             {
-                ST2a();
-                
+                AF2();
             }
             else if  (y[i] == 'E')
             { 
-                SV2a();
-                
+                R();
             }
             else if  (y[i] == 'F')
             {
-                SV2();
-                
+                R2();
             }
             else if  (y[i] == 'G')
             {
-                rt();
-                
+                AR();
             }
             else if  (y[i] == 'H')
             {
-                rta();
-                
+                AR2();
             }
             else if  (y[i] == 'I')
             {
-                SV1();
-                
+                B();
             }
             else if  (y[i] == 'J')
             {
-                SV1a();
-                
+                B2();
+            }
+            else if  (y[i] == 'K')
+            {
+                AB();
+            }
+            else if  (y[i] == 'L')
+            {
+                AB2();
+            }
+            else if  (y[i] == 'M')
+            {
+                L();
+            }
+            else if  (y[i] == 'N')
+            {
+                L2();
+            }
+            else if  (y[i] == 'O')
+            {
+                AL();
+            }
+            else if  (y[i] == 'P')
+            {
+                AL2();
+            }
+            else if  (y[i] == 'Q')
+            {
+                D();
+            }
+            else if  (y[i] == 'R')
+            {
+                D2();
+            }
+            else if  (y[i] == 'S')
+            {
+                AD();
+            }
+            else if  (y[i] == 'T')
+            {
+                AD2();
             }
           }
         }
